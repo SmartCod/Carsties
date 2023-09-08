b2e7db48dae74138a87c969580f423ec38bb923c
Daniel Hajek <danielhajek85@gmail.com>
2023-09-08 10:08:10 +0200

    end of section 11

---
+++ src/AuctionService/Services/GrpcAuctionService.cs
@@ -0,0 +1,35 @@
+﻿using AuctionService.Data;
+using Grpc.Core;
+
+namespace AuctionService;
+
+public class GrpcAuctionService : GrpcAuction.GrpcAuctionBase
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public GrpcAuctionService(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<GrpcAuctionResponse> GetAuction(GetAuctionRequest request,
+        ServerCallContext context)
+    {
+        Console.WriteLine("==> Received Grpc request from auction");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(request.Id))
+                        ?? throw new RpcException(new Status(StatusCode.NotFound, "Not found"));
+        var response = new GrpcAuctionResponse
+        {
+            Auction = new GrpcAuctionModel
+            {
+                Id = auction.Id.ToString(),
+                AuctionEnd = auction.AuctionEnd.ToString(),
+                ReservePrice = auction.ReservePrice,
+                Seller = auction.Seller,
+            }
+        };
+
+        return response;
+    }
+}
